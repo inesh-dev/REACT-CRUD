a0295dec39a4a722f4898bda2a15f5435e436494
inesh-dev <ishanpokhrel16@gmail.com>
2026-02-19 21:07:52 +0545

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // IMPORTANT for GitHub Pages: replace "grocery-bud" if your repo name is different
+  base: "/grocery-bud/",
+});
